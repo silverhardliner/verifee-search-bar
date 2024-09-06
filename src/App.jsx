5bc17536f54dc46044a5ddfe918e895cb3d1ac01
Matej Kopecky <kopecm28@fel.cvut.cz>
2024-09-06 11:30:52 +0200

    code for iframe resize

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,29 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import "./App.css";
 import { SearchBar } from "./components/SearchBar";
 import { SearchResultsList } from "./components/SearchResultsList";
 
 function App() {
+  const sendHeight = () => {
+    const height = document.documentElement.scrollHeight;
+    window.parent.postMessage({ iframeHeight: height }, "*");
+  };
+
+  useEffect(() => {
+    sendHeight();
+    window.addEventListener("resize", sendHeight);
+    return () => {
+      window.removeEventListener("resize", sendHeight);
+    };
+  }, []);
 
   const [results, setResults] = useState([]);
 
   return (
     <div className="App">
       <div className="search-bar-container">
-        <SearchBar setResults={setResults}/>
-        <SearchResultsList results={results}/>
+        <SearchBar setResults={setResults} />
+        <SearchResultsList results={results} />
       </div>
     </div>
   );
