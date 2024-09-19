2d71326c6580da53c407453eb013e4d6ca473498
Matej Kopecky <kopecm28@fel.cvut.cz>
2024-09-19 14:59:23 +0200

    fallback image

--- src/components/ImageIcon.jsx
+++ src/components/ImageIcon.jsx
@@ -4,11 +4,17 @@ import { Download, Block } from "akar-icons";
 import "./ImageIcon.css";
 
 export const ImageIcon = ({ result }) => {
+
+  const handleError = (e) => {
+    // Set fallback image when an error occurs
+    e.target.src = "https://picsum.photos/60/40";
+  };
+
   if (result.image_url) {
     result.image_url = result.image_url.replace("http:", "https:");
     return (
       <div className="img-cont">
-        <img src={result.image_url} alt={result.title} loading="lazy" />
+        <img src={result.image_url} alt={result.title} loading="lazy" onError={handleError}/>
       </div>
     );
   } else if (result.loading) {
